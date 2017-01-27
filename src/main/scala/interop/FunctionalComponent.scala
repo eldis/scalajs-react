1943b740c5c5cd5accbdc0d1f15166d097a84ae7
Anatoly Galiulin <galiulin.anatoly@gmail.com>
2017-01-27 13:43:54 +0700

    Improve FunctionalComponent type

--- src/main/scala/interop/FunctionalComponent.scala
+++ src/main/scala/interop/FunctionalComponent.scala
@@ -4,33 +4,31 @@ import scalajs.js
 import js.|
 
 @js.native
-trait FunctionalComponent[-P <: js.Any] extends js.Any
+trait FunctionalComponent[-P <: js.Any] extends js.Function1[P, ReactNode]
 
 object FunctionalComponent {
 
-  type Result = ReactNode
-
-  def apply[P <: js.Any](name: String)(f: Function1[P, Result]): FunctionalComponent[P] = {
-    var jf = f: js.Function1[P, Result]
+  def apply[P <: js.Any](name: String)(f: Function1[P, ReactNode]): FunctionalComponent[P] = {
+    var jf = f: js.Function1[P, ReactNode]
     jf.asInstanceOf[js.Dynamic].displayName = name
     jf.asInstanceOf[FunctionalComponent[P]]
   }
 
-  def apply[P <: js.Any](f: Function1[P, Result]): FunctionalComponent[P] =
+  def apply[P <: js.Any](f: Function1[P, ReactNode]): FunctionalComponent[P] =
     apply("FunctionalComponent")(f)
 
   @js.native
-  trait WithChildren[-P <: js.Any] extends js.Any
+  trait WithChildren[-P <: js.Any] extends js.Function2[P, PropsChildren, ReactNode]
 
-  def withChildren[P <: js.Any](name: String)(f: Function2[P, PropsChildren, Result]): WithChildren[P] = {
+  def withChildren[P <: js.Any](name: String)(f: Function2[P, PropsChildren, ReactNode]): WithChildren[P] = {
     var proxy = ((p: P) => {
       val children = p.asInstanceOf[js.Dynamic].children.asInstanceOf[PropsChildren]
       f(p, if (js.Array.isArray(children)) children else js.Array(children.asInstanceOf[ReactNode]))
-    }): js.Function1[P, Result]
+    }): js.Function1[P, ReactNode]
     proxy.asInstanceOf[js.Dynamic].displayName = name
     proxy.asInstanceOf[WithChildren[P]]
   }
 
-  def withChildren[P <: js.Any](f: Function2[P, PropsChildren, Result]): WithChildren[P] =
+  def withChildren[P <: js.Any](f: Function2[P, PropsChildren, ReactNode]): WithChildren[P] =
     withChildren("FunctionalComponent")(f)
 }
